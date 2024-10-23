1d68c70db1057b35e22b784b4888c04ce205a662
IshanakDK <idkosippili@gmail.com>
2024-10-23 21:58:24 +0530

    product crud UI done

--- src/component/Product/ProductTable.tsx
+++ src/component/Product/ProductTable.tsx
@@ -6,6 +6,12 @@ import TableContainer from "@mui/material/TableContainer";
 import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
+import { Product } from "./ProductPage";
+import { Button } from "@mui/material";
+import DeleteIcon from "@mui/icons-material/Delete";
+import EditIcon from "@mui/icons-material/Edit";
+import DeleteConfirmationDialog from "../common/DeleteConfirmationDialog";
+import { useState } from "react";
 
 const StyledTableCell = styled(TableCell)(({ theme }) => ({
   [`&.${tableCellClasses.head}`]: {
@@ -33,7 +39,14 @@ function createData(name: string, calories: number, fat: number) {
 
 const rows = [createData("Frozen yoghurt", 159, 6.0)];
 
-export default function ProductTable() {
+export default function ProductTable({
+  onRowClick,
+}: {
+  onRowClick: (product: Product) => void;
+}) {
+  const [open, setOpen] = useState(false);
+  const [itemName, setItemName] = useState("");
+
   return (
     <>
       <TableContainer component={Paper}>
@@ -45,16 +58,12 @@ export default function ProductTable() {
               <StyledTableCell align="right">Description</StyledTableCell>
               <StyledTableCell align="right">Price</StyledTableCell>
               <StyledTableCell align="right">Quantity</StyledTableCell>
+              <StyledTableCell align="center">Action</StyledTableCell>
             </TableRow>
           </TableHead>
           <TableBody>
             {rows.map((row) => (
-              <StyledTableRow
-                key={row.name}
-                sx={{
-                  cursor: "pointer",
-                }}
-              >
+              <StyledTableRow key={row.name}>
                 <StyledTableCell component="th" scope="row">
                   {row.name}
                 </StyledTableCell>
@@ -62,11 +71,52 @@ export default function ProductTable() {
                 <StyledTableCell align="right">{row.fat}</StyledTableCell>
                 <StyledTableCell align="right">{row.fat}</StyledTableCell>
                 <StyledTableCell align="right">{row.fat}</StyledTableCell>
+                <StyledTableCell
+                  align="center"
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    gap: "10px",
+                  }}
+                >
+                  <Button
+                    variant="outlined"
+                    startIcon={<EditIcon />}
+                    onClick={() => {
+                      onRowClick({
+                        id: 1,
+                        name: "Product 1",
+                        description: "Product 1 Description",
+                        price: 1000,
+                        quantity: 10,
+                      });
+                    }}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    variant="outlined"
+                    startIcon={<DeleteIcon />}
+                    onClick={() => {
+                      setItemName("Product 1");
+                      setOpen(true);
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </StyledTableCell>
               </StyledTableRow>
             ))}
           </TableBody>
         </Table>
       </TableContainer>
+      <DeleteConfirmationDialog
+        open={open}
+        handleClose={() => {
+          setOpen(false);
+        }}
+        itemName={itemName}
+      />
     </>
   );
 }
